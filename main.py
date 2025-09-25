cb3c972a90b466e045b3cdd6fdd4bbbd6160e614
Blueturtle162 <annalise.kinsey@ucas-edu.net>
2025-09-25 19:43:16 +0000

    Co-authored-by: Lecheconflakes <Lecheconflakes@users.noreply.github.com> Co-authored-by: pinksquishy4 <pinksquishy4@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,1 +1,42 @@
+#AK,EB,YS,JS 6th Text based adventure game
+
+#Eva Briggs background story, begining and ending.
+#Juliette Santacruz, forest part of storyline     
+print("You arrive in The Lost Woods, the first part of the triforce buried deep in the woods. To obtain this piece, you will need to light torches to find you way and find a key to enter throough the gates.")
+print("First, you need to find the torch to light your way.")
+looking = input("Where will you look, in the bushes, or  inside the great oak tree?:\n").strip().lower()
+if looking == ("bushes"):
+    print("You look in the bushes only to find a couple of berries, which you put in your bag, you then head over to look in the great oak tree finding the torch!")
+elif looking == ("great oak tree"):
+    print("you search inside the Great Oak Tree, finding the torch! congrats.")
+else:
+    
+print()P
+
+
+
+
+
+#Annalise Kinsey, 2nd part lake region.
+print ("You arrive at lake Hylia. The second piece of the triforce visble at the bottom of the lake. To obtain it you will need to dive down to the bottom of the lake and find the key to the ornate chest it is hidden in.")
+print ("The lake is hundreds of feet deep and you will need scuba gear to reach the bottom. You glance around and notice a dark cave but you slaos can see a waterfall that leads up to a higher plateu over looking the lake")
+cave_choice = input(print("Will you go inside the cave or climb the waterfall? Type left for the cave and right for the waterfall")).strip().lower()
+if cave_choice == ("right"):
+    print("You make your way toward the sheer rock face with water cascading down. You slowly begin to make you way up the slick black rocks. Several hours later you finally arrive at the top of the waterfall. You can see a river streaching")
+
+
+
+
+
+
+
+
+#Yaretzi Sanchez, the volcano part.print(" Congrats, you have made it to Death mountian")
+print("Congrats you have made it to the base of Death mountain!")
+print("you have already have gotten the first two pieces of the triforce. Now you will need the last piece.")
+print(" However you will need armor in order to make it up death mountain, where the last piece of the triforce is guared by a Hinox.")
+print("you see a small light coming from inside a little hut looking house on your right, and to your left you see a very faint path")
+choice_DM = input(print("would yoou like"))
+
+
 
