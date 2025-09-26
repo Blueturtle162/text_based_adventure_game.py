0ed9c788cd5ad8e11d13b64ee6dc6bfd93520a0c
Blueturtle162 <annalise.kinsey@ucas-edu.net>
2025-09-26 19:44:40 +0000

    Co-authored-by: Lecheconflakes <Lecheconflakes@users.noreply.github.com> Co-authored-by: pinksquishy4 <pinksquishy4@users.noreply.github.com>

--- main.py
+++ main.py
@@ -14,8 +14,9 @@ else:
 
 print("You have the torch to light your path, you need to find the Gates of Secrets leading to the triforce.")
 print("You light your torch, and see the fire flickering left but you also see a trail of rocks leading foward.")
-choice = input("Will you go foward and follow the rocks, (type fowar)
-
+choice = input("Will you follow the rocks and go foward, or will you follow the torch and go left? (Type foward or type left)")
+if choice == ("left"):
+    print("You  go left, following the torch... after 1 hour of long walking, you end up the gates but it is gaurded by an evil elf!")
 
 #Annalise Kinsey, 2nd part lake region.
 def invalid_input():
@@ -29,22 +30,29 @@ if cave_choice == ("right"):
     print ("You make your way toward the sheer rock face with water cascading down. You slowly begin to make you way up the slick black rocks. Several hours later you finally arrive at the top of the waterfall")
 elif:
     print("You plunge into the dark cave and through the light coming from the opening you can see that the cave drops off into a cliff. You back  out of the cave and make you way towards the waterfall instead. You make your way up to the top of the waterfall and at the top")
-    
-print ("you can see a river streaching for miles srrounded by a medow filled with wild flowers. As you glance around you see a sign that saying that there is a town a little less than a mile away. You still need to find the key to the tresure so you begin to look around.)" 
+else:
+    invalid_input
+print ("you can see a river streaching for miles srrounded by a medow filled with wild flowers. As you glance around you see a sign that saying that there is a town a little less than a mile away. You still need to find the key to the tresure so you begin to look around.") 
 print ("After hours of looking you finally stumble upon a door that leads down into the earth. You open the door and decend into the darkness beyond. After stumbling down some stairs in the darkness you come upon a room with a lit torch. You aproch a wall and notice a riddle writen upon it.")
 print ("The riddle reads: I am tall when I am yound and I am short when I am old what am I?")
 riddle_answer = user_input(input(print("What does the riddle mean?")))
 
+#while True:
 if riddle_answer = ("candle") or ("a candle")
     print("Congradulations you solved the riddle.")
-else
-
+    break
+else:
+    print("You enter your answer to the riddle and nothing happens. You should try again.")
+print("The wall the riddle was written on begins to shake and move to the left. It reveals a room with a chest in the center. You open the chest and discover an small sliver key. You pocket the key and then exit the room. You make your way back up the stairs and exit into the medow")
+print("You still need to find scuba gear to get to the bottom of the lake. You rember seeing a sign about a town nearby and you amek your way down a dirt path that appears to lead to the nearest town. After walking for 20 minutes you stumble upon a small town with quiant little shops all lining a bussling town sqaure.")
+print("You examine all the shops and notice that one ")
 
 
 
 
 
 #Yaretzi Sanchez, the volcano part.print(" Congrats, you have made it to Death mountian")
+
 print("Congrats you have made it to the base of Death mountain!")
 print("you have already have gotten the first two pieces of the triforce. Now you will need the last piece.")
 print(" However you will need armor in order to make it up death mountain, where the last piece of the triforce is guared by a Hinox.")
@@ -54,6 +62,8 @@ if choice_DM==("left"):
     print("you go down the faint path. it had lots of preety colorful flowers and in one of the rose bushes you see something. You find a bottle with a  key and a small old map leading to somewhere inside.")
 else:
     print("you make your way to the small hut. you find nothing and go down the path" )
+
+print("after you walk down the path and collect the key and map you make your way following the map you fnd a hi")    
 
    
 
